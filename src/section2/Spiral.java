c5c2371b5abff1e4688686e0fba4c1e5d93c5671
League Student <league@iMac-11.attlocal.net>
2019-08-23 14:47:00 -0700

    day 5

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -16,30 +16,9 @@ public class Spiral {
 		N365.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
 		for (int i = 0; true; i++) {
-			if (i % 7 == 0) {
-				// 7. Change the pen color to random
-				N365.setPenColor(Color.RED);
-			} else if (i % 7 == 1) {
-
-				N365.setPenColor(Color.ORANGE);
-			} else if (i % 7 == 2) {
-
-				N365.setPenColor(Color.YELLOW);
-			} else if (i % 7 == 3) {
-
-				N365.setPenColor(Color.green);
-			} else if (i % 7 == 4) {
-
-				N365.setPenColor(Color.BLUE);
-			} else if (i % 7 == 5) {
-
-				N365.setPenColor(Color.magenta);
-			} else if (i % 7 == 6) {
-				N365.setPenColor(new Color (183,14,196));
-
-			}
+		N365.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			N365.move 1 * i);
+			N365.move(i);
 			// 2. Turn the robot 360/3 degrees to the right
 			N365.turn(360 / 7);
 			// 8. Set the pen width to i
